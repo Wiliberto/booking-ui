8ca6dab8fabd0060edd502633f9951f67d5d9516
Rodrigo Cabrera <rodrigo_cabrera_cab@hotmail.com>
2022-06-01 15:34:20 -0500

    chore: integration

--- packages/booking/src/index.js
+++ packages/booking/src/index.js
@@ -1,5 +1,5 @@
 export { Reservation } from './components/ReservationView';
-export { Booking } from './components/helloWorld';
+export { Booking } from './components/Home';
 export {DatePicker} from './components/DatePickerCheck/DatePicker';
 export {DatePickerButton} from './components/DatePickerCheck/DatePickerButtonCheck';
 export {DatePickerDescription} from './components/DatePickerCheck/DatePickerDescriptions';
