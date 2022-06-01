9bc5d50ad88ea2c26d27df538a285b07c7719d4b
Rodrigo Cabrera <rodrigo_cabrera_cab@hotmail.com>
2022-06-01 11:01:03 -0500

    changes to the context made

--- packages/booking/src/components/homeScreen.jsx
+++ packages/booking/src/components/homeScreen.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import { BookingContext } from './BookingContext';
 
 import DatePicker from './DatePickerCheck/DatePicker';
 
 export const Booking = () => {
   return (
-    <>
+    <BookingContext.Provider value={{}}>
       <DatePicker />
-    </>
+    </BookingContext.Provider>
   );
 };
